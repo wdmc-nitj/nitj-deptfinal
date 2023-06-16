bf5d329b43a50a406304d5ccbb29d9074b1a92ed
nitjofficial <78634968+nitjofficial@users.noreply.github.com>
2023-06-16 10:54:25 +0530

    department

--- src/config/server.js
+++ src/config/server.js
@@ -5,7 +5,7 @@ const departments = {
     "bt": "Bio Technology",
     "ce": "Civil Engineering",
     "ch": "Chemical Engineering",
-    "ec": "Electronics And Communication Engineering",
+    "ece": "Electronics And Communication Engineering",
     "ee": "Electrical Engineering",
     "ice": "Instrumentation And Control Engineering",
     "me": "Mechanical Engineering",
