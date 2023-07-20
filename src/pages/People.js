66ec5055d2fa52281a491cfa15d57ccffa77f0fa
Aditya Garg <uber6707@gmail.com>
2023-07-21 01:38:24 +0530

    changes to copy and about

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import defaultImg from '../components/Img/default_avatar.jpg'
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Heading from '../components/Heading';
 
 function People({ Title, Data }) {
     const url = useLocation();
     const dept = url.pathname.split('/')[2];
     const page = url.pathname.split('/')[3];
-
+    const navigate = useNavigate();
 
     return (
         <div className='w-[96%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-2 mx-auto my-[60px] pt-[54px] place-items-center'>
@@ -19,8 +19,7 @@ function People({ Title, Data }) {
                 {
                     Data ? Data.map((item, i) => {
                         return (
-                            <Link to={page === "Faculty" ? { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}/${item._id}`, state: i } : { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}`, state: i }}>
-                                <div key={i} className="flex flex-col flex-grow content-center h-full justify-center mt-0 text-inherit shadow hover:shadow-lg hover:shadow-blue-500 shadow-blue-400 rounded-lg my-4">
+                                <div key={i} className="flex flex-col flex-grow content-center h-full justify-center mt-0 text-inherit shadow hover:shadow-lg hover:shadow-blue-500 shadow-blue-400 rounded-lg my-4 cursor-pointer" onClick={() => navigate(page === "Faculty" ? { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}/${item._id}`, state: i } : { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}`, state: i })}>
                                     <div className="flex flex-col sm:flex-row border-t-0 text-[rgba(0,105,140,1)] py-3 text-left text-base items-center content-center px-[14px] xl:px-[18px]">
                                         <div className="w-32 h-32 xl:w-36 xl:h-36 flex-grow-0 flex-shrink-0">
                                             {
@@ -28,14 +27,13 @@ function People({ Title, Data }) {
                                                 <img src={defaultImg} className="w-full h-full object-fill object-center rounded-full shadow-xl border flex-grow-0 flex-shrink-0 opacity-70" alt='...' />
                                             }
                                         </div>
-                                        <div className="flex flex-col flex-grow leading-8 md:mr-2 xl:mr-5 text-center sm:text-start md:justify-start mt-8 md:mt-0 sm:ml-9 md:mb-0">
-                                            <div className="text-[22.5px]">{item?.name}</div>
-                                            <div className="font-semibold text-[rgba(0,0,0,0.7)] tracking-wide">{item?.position}</div>
-                                            <div className="font-medium text-[rgba(0,0,0,0.7)] text-blue-500 tracking-wide"><span className='text-blue-700'>{item?.email?'Email:-':''}</span> {item?.email}</div>
+                                        <div className="flex flex-col flex-grow leading-8 md:mr-2 xl:mr-5 text-center sm:text-start md:justify-start mt-8 md:mt-0 sm:ml-9 md:mb-0" onClick={(e) => window.getSelection().toString().length > 0 ? e.stopPropagation() : () => {}}>
+                                            <div className="text-[22.5px] cursor-text">{item?.name}</div>
+                                            <div className="font-semibold text-[rgba(0,0,0,0.7)] tracking-wide cursor-text">{item?.position}</div>
+                                            <div className="font-medium text-[rgba(0,0,0,0.7)] text-blue-500 tracking-wide cursor-text"><span className='text-blue-700'>{item?.email?'Email:-':''}</span> {item?.email}</div>
                                         </div>
                                     </div>
                                 </div>
-                            </Link>
                         )
                     }) : <h1>Data not Available</h1>
                 }
