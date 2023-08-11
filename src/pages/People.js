30d8483d98531118fbb9c1e731d7a79b4ce5e42a
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2023-08-11 16:20:20 +0530

    Revert "Added View Profile link to Faculty Profile."
    This reverts commit 5968bc969e17b6b7f36297bfe75547b23795894a.

--- src/pages/People.js
+++ src/pages/People.js
@@ -19,29 +19,24 @@ function People({ Title, Data }) {
                 {
                     Data ? Data.map((item, i) => {
                         return (
-                            <div key={i} className="flex flex-col flex-grow content-center h-full justify-center mt-0 text-inherit shadow hover:shadow-lg hover:shadow-blue-500 shadow-blue-400 rounded-lg my-4 cursor-pointer">
-                                <div className="flex flex-col sm:flex-row border-t-0 text-[rgba(0,105,140,1)] py-3 text-left text-base items-center content-center px-[14px] xl:px-[18px]">
-                                    <div className="w-32 h-32 xl:w-36 xl:h-36 flex-grow-0 flex-shrink-0">
-                                        {
-                                            item?.img ? <img src={item?.img} className="w-full h-full object-fill object-center rounded-full shadow-xl border flex-grow-0 flex-shrink-0" alt='...' /> :
+                                <div key={i} className="flex flex-col flex-grow content-center h-full justify-center mt-0 text-inherit shadow hover:shadow-lg hover:shadow-blue-500 shadow-blue-400 rounded-lg my-4 cursor-pointer" onClick={() => navigate(page === "Faculty" ? { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}/${item._id}`, state: i } : { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}`, state: i })}>
+                                    <div className="flex flex-col sm:flex-row border-t-0 text-[rgba(0,105,140,1)] py-3 text-left text-base items-center content-center px-[14px] xl:px-[18px]">
+                                        <div className="w-32 h-32 xl:w-36 xl:h-36 flex-grow-0 flex-shrink-0">
+                                            {
+                                                item?.img ? <img src={item?.img} className="w-full h-full object-fill object-center rounded-full shadow-xl border flex-grow-0 flex-shrink-0" alt='...' />:
                                                 <img src={defaultImg} className="w-full h-full object-fill object-center rounded-full shadow-xl border flex-grow-0 flex-shrink-0 opacity-70" alt='...' />
-                                        }
-                                    </div>
-                                    <div className="flex flex-col flex-grow leading-8 md:mr-2 xl:mr-5 text-center sm:text-start md:justify-start mt-8 md:mt-0 sm:ml-9 md:mb-0" >
-                                        <div className="text-[22.5px] cursor-text">{item?.name}</div>
-                                        <div className="font-semibold text-[rgba(0,0,0,0.7)] tracking-wide cursor-text">{item?.position}</div>
-                                        <div className="font-medium text-[rgba(0,0,0,0.7)] text-blue-500 tracking-wide cursor-text"><span className='text-blue-700'>{item?.email ? 'Email:-' : ''}</span> {item?.email}</div>
-                                        {Title === "Faculty" ? (
-                                            <div className="text-[15px] border-none pr-2  cursor-pointer hover:underline" onClick={() => navigate(page === "Faculty" ? { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}/${item._id}`, state: i } : { pathname: `/dept/${dept}/${url.pathname.split('/').pop()}`, state: i })}>View Profile</div>
-                                        ) : null}
-
+                                            }
+                                        </div>
+                                        <div className="flex flex-col flex-grow leading-8 md:mr-2 xl:mr-5 text-center sm:text-start md:justify-start mt-8 md:mt-0 sm:ml-9 md:mb-0" onClick={(e) => window.getSelection().toString().length > 0 ? e.stopPropagation() : () => {}}>
+                                            <div className="text-[22.5px] cursor-text">{item?.name}</div>
+                                            <div className="font-semibold text-[rgba(0,0,0,0.7)] tracking-wide cursor-text">{item?.position}</div>
+                                            <div className="font-medium text-[rgba(0,0,0,0.7)] text-blue-500 tracking-wide cursor-text"><span className='text-blue-700'>{item?.email?'Email:-':''}</span> {item?.email}</div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                         )
                     }) : <h1>Data not Available</h1>
                 }
-
             </div>
         </div>
     )
