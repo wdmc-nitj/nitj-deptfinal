47015d32ec00d3c6925416377ccac634f92c4f90
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2023-11-19 11:07:23 +0530

    Update ResearchLab.js

--- src/pages/ResearchLab.js
+++ src/pages/ResearchLab.js
@@ -3,44 +3,36 @@ import { useLocation } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import Heading from '../components/Heading';
 import Loading from '../components/Loading';
-import { useState } from 'react';
-import PopupCard from '../components/PopupCard';
-
+import { Link } from 'react-router-dom';
 function ResearchLab() {
   const url = useLocation();
   const dept = url.pathname.split('/')[2];
   const { data, loading } = useFetch(`/dept/${dept}/Infrastructure?q=Research Labs`);
 
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null); 
-
-  const openPopup = (item) => {
-    setIsPopupOpen(true);
-    setSelectedItem(item); 
-  };
-
-  const closePopup = () => {
-    setIsPopupOpen(false);
-    setSelectedItem(null); 
-  };
+  
 
   return (
     <div className='w-[96%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-2 mx-auto my-[60px] pt-[54px] place-items-center'>
       <Heading name="Research Labs" />
-      {!loading?<div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 sm:m-4 sm:p-2 place-items-center gap-4 antialiased text-gray-900">
-        {
-          data && data.map((item, i) => {
-            return (
-              item.type === "Research Labs" && <div key={i} className='w-full my-4 sm:mx-4 p-2'>
-                <div className="w-full h-60 sm:h-72 border-2 object-cover object-center relative rounded-lg shadow-md bg-grey overflow-hidden group">
+      {!loading ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 sm:m-4 sm:p-2 place-items-center gap-4 antialiased text-gray-900">
+          {data && data.length > 0 ? (
+            data.map((item, i) => {
+              return (
+                item.type === "Research Labs" && (
+                  <div key={i} className='w-full my-4 sm:mx-4 p-2'>
+                    <div className="w-full h-60 sm:h-72 border-2 object-cover object-center relative rounded-lg shadow-md bg-grey overflow-hidden group">
                       <img
                         src={item?.img}
-                        alt="Research Labs"
+                        alt="Research Area"
                         className="w-full h-full object-cover object-center rounded-lg group-hover:scale-110 shadow-md duration-500"
                       />
-                      {item.imgdesp ? <div className="absolute cursor-pointer z-10 p-2 inset-0 flex items-center justify-center text-xl font-bold text-white bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 duration-300 transition" onClick={() => openPopup(item)}>
+                      {item.imgdesp ? (
+                        <Link to={`/dept/${dept}/Imgdesp/${item._id}`} className="absolute cursor-pointer z-10 p-2 inset-0 flex items-center justify-center text-xl font-bold text-white bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 duration-300 transition">
                         {item?.title && item.title.length > 40 ? `${item.title.slice(0, 40)}...` : item.title}
-                      </div>:null}
+                      </Link>
+                      
+                      ) : null}
                     </div>
 
                     <div className=" z-20 relative px-4 -mt-7 md:-mt-9">
@@ -48,16 +40,17 @@ function ResearchLab() {
                         <h4 title={item?.title} className="text-xl cursor-pointer font-semibold uppercase leading-tight truncate">{item?.title}</h4>
                       </div>
                     </div>
-
-                    
-                    {selectedItem && isPopupOpen && selectedItem === item && (
-                      <PopupCard isOpen={isPopupOpen} onClose={closePopup} title={selectedItem?.title} description={selectedItem?.imgdesp} />
-                    )}
                   </div>
-            )
-          })
-        }
-      </div>:<Loading/>}
+                )
+              );
+            })
+          ) : (
+            <h1>Data not Available</h1>
+          )}
+        </div>
+      ) : (
+        <Loading />
+      )}
       
     </div>
   )
