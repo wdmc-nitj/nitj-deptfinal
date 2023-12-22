6601da067ed3bedf61ee2bd33225d5afb1d58c72
Parteek Goyal <parteekgoyal118@gmail.com>
2023-12-22 10:25:49 +0530

    Update ImgDesp.js

--- src/pages/ImgDesp.js
+++ src/pages/ImgDesp.js
@@ -6,7 +6,7 @@ import Loading from "../components/Loading";
 import useFetch from "../hooks/useFetch";
 import OpenPdf from "./OpenPdf";
 
-function LabSyllabus({data , loading}){
+function LabSyllabus({data}){
   const [state,setState]= useState("");
   useEffect(()=>{
       setState(data[0]?.link);
@@ -15,7 +15,7 @@ function LabSyllabus({data , loading}){
   return (
       <div className='w-[98%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-2 mx-1 xl:mx-3 my-[60px] pt-[54px] place-items-center'>
           <Heading name="Lab Manual" />
-          {!loading?<div className='shadow shadow-blue-400 md:m-4 pb-2'>
+          <div className='shadow shadow-blue-400 md:m-4 pb-2'>
               <div className='flex items-center w-full py-3 font-medium text-lg px-4 shadow-md shadow-blue-200'>
                   <div className='flex w-fit items-center border border-gray-300 text-gray-900 text-sm p-2 rounded'>
                       <label htmlFor="states" className="mr-2">Programme :</label>
@@ -29,7 +29,7 @@ function LabSyllabus({data , loading}){
                   </div>
               </div>
               <OpenPdf link={state} />
-          </div>:<Loading/>}
+          </div>
           
       </div>
   )
@@ -80,7 +80,7 @@ function ImgDesp() {
         return <ImageCarousel data={image} />
       }
       )}
-     { data?.syllabus && <LabSyllabus loading={loading} data={data?.syllabus}/>}
+     { data?.syllabus && <LabSyllabus data={data?.syllabus}/>}
     </div>
   );
 }
