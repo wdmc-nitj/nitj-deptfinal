9da495dcbd23d82bb85cd55d5c5e155921e8089d
Parteek Goyal <parteekgoyal118@gmail.com>
2024-02-26 22:59:13 +0530

    added faculty profile edit option

--- src/forms/facultyprofile/PersonalDetails.js
+++ src/forms/facultyprofile/PersonalDetails.js
@@ -117,11 +117,11 @@ function PersonalDetails({ edit, data, token }) {
                                     </div>
                                 </div>
                                 <div className="flex px-3 w-full justify-end">
-                                    <div onClick={handleSubmit} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 active:translate-y-[2px] hover:shadow-xl">
+                                    <button onClick={handleSubmit} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 active:translate-y-[2px] hover:shadow-xl">
                                         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md">
                                             Submit
                                         </span>
-                                    </div>
+                                    </button>
                                 </div>
                             </form>
                         </div>
