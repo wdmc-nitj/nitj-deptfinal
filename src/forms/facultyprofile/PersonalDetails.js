b52b8fb2e3c6d6d554095bc860031456c274b394
Parteek Goyal <parteekgoyal118@gmail.com>
2024-02-26 22:44:20 +0530

    added faculty profile edit option

--- src/forms/facultyprofile/PersonalDetails.js
+++ src/forms/facultyprofile/PersonalDetails.js
@@ -6,33 +6,30 @@ import axios from 'axios';
 function PersonalDetails({ edit, data, token }) {
     const tablehead = ['Name', 'Designation', 'Department', 'Qualification', 'Address', 'Phone', 'Email ID'];
     const feild = ['name', 'designation', 'department', 'education_qualification'];
-    const [education, setEducation] = useState({
-        'column':'',
-        'clg':'',
-        'degree':'',
-        'field':'',
-        'year':''
+    const qualificationMapping = {
+        'column': 'column',
+        'College': 'clg',
+        'Degree': 'degree',
+        'field': 'field',
+        'Year': 'year'
+    }
 
-    })
+    const [education, setEducation] = useState(data?.education_qualification)
+    const [editAddress, setEditAddress] = useState(data.correspondence_address)
+    const [editImg, setEditImg] = useState(data.img)
     const dept = useLocation().pathname.split('/')[2];
-    const address = ['address1', 'address2', 'city', 'pin', 'state']
-    const Phone = data['address'] ? data['address']['phone'] : ""
-    const Fax = data['address'] ? data['address']['fax'] : ""
+    const address = ['address', 'city', 'pin', 'state']
     const handleSubmit = async (e) => {
 
-        let newRow = {};
-        const formdata = new FormData(e.target);
-        for (let [key, value] of formdata.entries()) {
-            newRow = {
-                ...newRow,
-                [key]: value
-            }
+        let data = {
+            correspondence_address: editAddress,
+            img: editImg,
+            education_qualification: education,
         }
         try {
-            await axios.put(`${SERVER_URL}/dept/${dept}/Faculty/${data._id}/${token}?q=personal_link`, newRow);
+            await axios.put(`${SERVER_URL}/dept/${dept}/Faculty/${data._id}/${token}/personalDetails`, data);
         } catch (error) {
-            //console.log(error);
-            alert('Some error occured please try again after sometime')
+            alert('Some error occured. Use Chrome browser to edit Details.')
         }
     }
     return (
@@ -44,31 +41,50 @@ function PersonalDetails({ edit, data, token }) {
                             <form>
                                 <div className="overflow-hidden sm:rounded-md">
                                     <div className="bg-white px-4 py-5 sm:p-6">
-                                        <div className="grid grid-cols-6 gap-6">
-                                            <div>Education Qualification</div>
+                                        <div className='block'>Education Qualification</div>
+                                        <div className="flex flex-col gap-6">
                                             {
-
-                                                // <div key={i} className="col-span-6 sm:col-span-3">
-                                                //     <label htmlhtmlFor="last-name" className="block uppercase text-sm font-medium px-1">{item}</label>
-                                                //     <input type="text" name={item} className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm"/>
-                                                // </div>
-
+                                                education.map((ed, index) => {
+                                                    return (
+                                                        <div className='flex gap-2'>
+                                                            {Object.keys(qualificationMapping).map((feild) => {
+                                                                return (
+                                                                    <div key={index} className="col-span-6 sm:col-span-3">
+                                                                        <label htmlFor={qualificationMapping[feild]} className="block uppercase text-sm font-medium px-1">{feild}</label>
+                                                                        <input type="text" name={qualificationMapping[feild]} className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm" value={ed[feild]} />
+                                                                    </div>
+                                                                )
+                                                            })}
+                                                            <div className='bg-[#0054A6] ml-2 text-white text-base duration-500 w-20 xl:w-24 py-2 px-3 text-center shadow-md border border-[#FFD66E]  rounded hover:-translate-y-1 hover:scale-110' onClick={() => {
+                                                                setEducation(prev => {
+                                                                    return prev.filter((val, i) => i !== index)
+                                                                })
+                                                            }}>Delete</div>
+                                                        </div>
+                                                    )
+                                                })
                                             }
-                                            <button className='' onClick={() => setEducation(prev => {
-                                                return [...prev,{'column':'',
-                                                'clg':'',
-                                                'degree':'',
-                                                'field':'',
-                                                'year':''}]
-                                            })}>Add</button>
                                         </div>
+                                        <div className='bg-[#0054A6] mt-2 ml-2 text-white text-base duration-500 w-20 xl:w-24 py-2 px-3 text-center shadow-md border border-[#FFD66E]  rounded hover:-translate-y-1 hover:scale-110' onClick={() => setEducation(prev => {
+                                            return [...prev, {
+                                                'column': '',
+                                                'clg': '',
+                                                'degree': '',
+                                                'field': '',
+                                                'year': ''
+                                            }]
+                                        })}>Add</div>
                                         <div className="grid grid-cols-6 gap-6 mt-4">
                                             {
                                                 address?.map((item, i) => {
                                                     return (
                                                         <div key={i} className="col-span-6 sm:col-span-3">
                                                             <label htmlhtmlFor="last-name" className="block uppercase text-sm font-medium px-1">{item}</label>
-                                                            <textarea type="text" name={item} className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm"></textarea>
+                                                            <textarea type="text" onChange={(e) => {
+                                                                setEditAddress(prev => {
+                                                                    return { ...prev, [item]: e.target.value }
+                                                                })
+                                                            }} name={item} className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm"></textarea>
                                                         </div>
                                                     )
                                                 })
@@ -77,21 +93,35 @@ function PersonalDetails({ edit, data, token }) {
                                         <div className='grid grid-cols-6 gap-6 mt-4'>
                                             <div className="col-span-6 sm:col-span-3">
                                                 <label htmlhtmlFor="last-name" className="block uppercase text-sm font-medium px-1">Phone</label>
-                                                <textarea type="text" className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm"></textarea>
+                                                <textarea type="text" name='phone' onChange={(e) => {
+                                                    setEditAddress(prev => {
+                                                        return { ...prev, phone: e.target.value }
+                                                    })
+                                                }} className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm" value={editAddress?.phone}></textarea>
                                             </div>
                                             <div className="col-span-6 sm:col-span-3">
                                                 <label htmlhtmlFor="last-name" className="block uppercase text-sm font-medium px-1">Fax</label>
-                                                <textarea type="text" className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm"></textarea>
+                                                <textarea type="text" name='fax' onChange={(e) => {
+                                                    setEditAddress(prev => {
+                                                        return { ...prev, fax: e.target.value }
+                                                    })
+                                                }} value={editAddress?.fax} className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm"></textarea>
+                                            </div>
+                                            <div className="col-span-6 sm:col-span-3">
+                                                <label htmlhtmlFor="last-name" className="block uppercase text-sm font-medium px-1">Image</label>
+                                                <input type="text" name='img' onChange={(e) => {
+                                                    setEditImg(e.target.value)
+                                                }} value={editAddress?.fax} className="appearance-none bg-white py-2 px-3 mt-1 block border w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 focus:border-2 sm:text-sm"/>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="flex px-3 w-full justify-end">
-                                    <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 active:translate-y-[2px] hover:shadow-xl">
+                                    <div onClick={handleSubmit} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 active:translate-y-[2px] hover:shadow-xl">
                                         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md">
                                             Submit
                                         </span>
-                                    </button>
+                                    </div>
                                 </div>
                             </form>
                         </div>
@@ -129,17 +159,17 @@ function PersonalDetails({ edit, data, token }) {
                                             )
                                         })
                                     }
-                                    {/* <tr>
-                                    <td className="w-48 align-top font-bold pr-4 pl-2 py-2">Address</td>
-                                    <td className="align-top font-bold pr-4 pl-2 py-2">:</td>
-                                    <td className='align-top pr-4 pl-2 py-2'>
-                                        {
-                                            address.map((item, i) => {
-                                                return <span className='mx-1' key={i}>{data['address']?data['address'][item]:""}</span>
-                                            })
-                                        }
-                                    </td>
-                                </tr> */}
+                                    <tr>
+                                        <td className="w-48 align-top font-bold pr-4 pl-2 py-2">Address</td>
+                                        <td className="align-top font-bold pr-4 pl-2 py-2">:</td>
+                                        <td className='align-top pr-4 pl-2 py-2'>
+                                            {
+                                                address.map((item, i) => {
+                                                    return <span className='mx-1' key={i}>{editAddress ? editAddress[item] : ""}</span>
+                                                })
+                                            }
+                                        </td>
+                                    </tr>
                                     <tr>
                                         <td className="w-48 align-top font-bold pr-4 pl-2 py-2">Email</td>
                                         <td className="align-top font-bold pr-4 pl-2 py-2">:</td>
@@ -148,7 +178,7 @@ function PersonalDetails({ edit, data, token }) {
                                     <tr>
                                         <td className="w-48 align-top font-bold pr-4 pl-2 py-2">Phone</td>
                                         <td className="align-top font-bold pr-4 pl-2 py-2">:</td>
-                                        <td className='align-top pr-4 pl-2 py-2'>{Phone}</td>
+                                        <td className='align-top pr-4 pl-2 py-2'>{editAddress?.phone}</td>
                                     </tr>
                                 </tbody>
                             </table>
