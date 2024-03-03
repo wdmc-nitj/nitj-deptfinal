9249cc7d15c66dafe3516a5499d8acea56229c71
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2024-03-03 20:21:12 +0530

    Courses Updated

--- src/pages/PMKVY.js
+++ src/pages/PMKVY.js
@@ -1,31 +1,22 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Heading from '../components/Heading';
-import Loading from '../components/Loading';
-import PopupCard from '../components/PopupCard';
 
 const staticData = [
   {
     type: 'MSME',
-    title: 'Computer Lab',
+    title: 'Web Development Course',
     image: 'image1.jpg',
     _id: '1',
   },
   {
     type: 'MSME',
-    title: 'Data Lab',
+    title: 'AI/ML course',
     image: 'image2.jpg',
     _id: '2',
   },
 ];
 
-function PMKVY() {
-  const navigate = useNavigate();
-
-  const handleImageClick = (item) => {
-    navigate(`/center/department/Imgdesp/${item._id}`);
-  };
-
+function MSME() {
   return (
     <div className='w-[96%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-2 mx-auto mt-[60px] pt-[54px] place-items-center'>
       <Heading name="PMKVY" />
@@ -37,17 +28,17 @@ function PMKVY() {
               <div className="w-full h-60 sm:h-72 border-2 object-cover object-center relative rounded-lg shadow-md bg-grey overflow-hidden group" >
                 <img
                   src={item.image}
-                  alt="Department Labs"
-                  className="w-full h-full object-cover object-center rounded-lg group-hover:scale-110 shadow-md duration-500"
+                  alt="Courses"
+                  className="w-full h-full object-cover object-center rounded-lg shadow-md duration-500"
                 />
-                {item.title && <div className="absolute cursor-pointer z-10 p-2 inset-0 flex items-center justify-center text-xl font-bold text-white bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 duration-300 transition" onClick={() => handleImageClick(item)}>
+                {item.title && <div className="absolute z-10 p-2 inset-0 flex items-center justify-center text-xl font-bold text-white bg-black bg-opacity-40 opacity-0 duration-300 transition">
                   {item.title}
                 </div>}
               </div>
 
               <div className=" z-20 relative px-4 -mt-7 md:-mt-9">
                 <div className="max-w-fit mx-auto bg-white flex items-center  justify-center p-4 sm:p-6 rounded-lg shadow-lg">
-                  <h4 title={item.title} className="text-xl cursor-pointer font-semibold uppercase leading-tight truncate">{item.title}</h4>
+                  <h4 title={item.title} className="text-xl font-semibold uppercase leading-tight truncate">{item.title}</h4>
                 </div>
               </div>
             </div>
@@ -58,4 +49,4 @@ function PMKVY() {
   );
 }
 
-export default PMKVY;
+export default MSME;
