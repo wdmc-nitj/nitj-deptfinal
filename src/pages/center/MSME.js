b81008c41519d477e86a0f8968ef0cb282651d64
Yash-032 <yashag0910@gmail.com>
2024-04-20 20:46:09 +0530

    MSME Data Entry

--- src/pages/center/MSME.js
+++ src/pages/center/MSME.js
@@ -12,23 +12,23 @@ const data = [
   {
     'S.No': '1',
     'Department': 'Hm',
-    'Course Name': 'Advanced MDP',
+    'Course Name': 'Advanced MDP/Management of Startups and Business',
     'Duration': '12 Feb 2024 - Feb 16 2024',
-    'No. of Students': '11',
+    'No. of Students': '25',
     'Link': '',
   },
   {
     'S.No': '2',
     'Department': 'Hm',
-    'Course Name': 'Advanced MDP',
+    'Course Name': 'Advanced MDP/Finance for Non-Finance',
     'Duration':'29 Jan 2024 - Feb 2 2024',
-    'No. of Students': '20',
+    'No. of Students': '25',
     'Link': '',
   },
   {
     'S.No': '3',
     'Department': 'CY',
-    'Course Name': 'Advanced E-SDP',
+    'Course Name': 'Advanced E-SDP/ Skill Development in Post Harvest Technology and Natural Farming for Unemployed Youth',
     'Duration': '29 Jan 2023 - 03 Feb 2024',
     'No. of Students': '25',
     'Link': '',
@@ -37,16 +37,16 @@ const data = [
   {
     'S.No': '4',
     'Department': 'IPE',
-    'Course Name': 'Advanced MDP',
+    'Course Name': 'Advanced MDP/Productivity Enhancement Soft Tools for MSMEs Managers/Owners',
     'Duration': '25 Jan 2024 - 30 January 2024',
-    'No. of Students': '18',
+    'No. of Students': '25',
     'Link': '',
   }
   ,
   {
     'S.No': '5',
     'Department': 'Ch',
-    'Course Name': 'Advanced E-SDP',
+    'Course Name': 'Advanced E-SDP/Opportunities for MSMEs in Solid Waste Management',
     'Duration': '27 Nov 2023 - 01 Dec 2023',
     'No. of Students': '25',
     'Link': '',
@@ -55,27 +55,27 @@ const data = [
   {
     'S.No': '6',
     'Department': 'EE',
-    'Course Name': 'Advanced E-SDP',
+    'Course Name': 'Advanced E-SDP/ Installation and Commissioning of Solar Plants for Net Zero Energy Buildings',
     'Duration': '07 December 2023 - 11 December 2023',
-    'No. of Students': '20',
+    'No. of Students': '25',
     'Link': '',
   }
   ,
   {
     'S.No': '7',
     'Department': 'ICE',
-    'Course Name': 'Advanced E-SDP',
+    'Course Name': 'Advanced E-SDP/ Recent Trends Challenges in Instrumentation and Control for MSME Industry',
     'Duration': '04 Dec 2023 - 06 Dec 2024',
-    'No. of Students': '28',
+    'No. of Students': '25',
     'Link': '',
   }
   ,
   {
     'S.No': '8',
     'Department': 'IPE',
-    'Course Name': 'Advanced E-SDP',
+    'Course Name': 'Advanced E-SDP/ Additive Manufacturing of Polymers Scope of Entrepreneurship',
     'Duration': '16 Dec 2023 - 20 Dec 2023',
-    'No. of Students': '11',
+    'No. of Students': '25',
     'Link': '',
   }
 ];
@@ -83,7 +83,7 @@ const data = [
 function MSME() {
   return (
     <div className='w-[96%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-2 mx-auto mt-[60px] pt-[54px] place-items-center'>
-      <Heading name="PMKVY" />
+      <Heading name="MSME" />
       
       <BaseTable tablehead={table.thead} feild={table.feild} data={data}/>
     </div>
