077d113b08e4040993860f26d3aaad1ae77acd74
nitjofficial <78634968+nitjofficial@users.noreply.github.com>
2026-04-16 14:37:31 +0530

    Update Consultancy.js

--- src/pages/Consultancy.js
+++ src/pages/Consultancy.js
@@ -6,23 +6,23 @@ import {useParams } from 'react-router-dom'
 import Loading from '../components/Loading'
 
 function Consultancy() {
-    const tablehead = ['Role', "Consultancy Type", "Title", "Funding Agency", "From","To","Amount","Status","Co-investigator"]
-    const dept= useParams()?.dept;
-    const {data,loading} = useFetch(`/dept/${dept}/Consultancy`);
+    // Updated tablehead to match actual field names from your MongoDB document
+    const tablehead = ["NAME OF FACULTY (CHIEF CONSULTANT)", "CLIENT ORGANIZATION", "TITLE OF CONSULTANCY OF PROJECT", "AMOUNT RECEIVED (IN RUPEES)", "AMOUNT RECEIVED (IN WORDS)"]
+    const dept = useParams()?.dept;
+    const {data, loading} = useFetch(`/dept/${dept}/Consultancy`);
 
-    let consultancies = []
-    data.map((ele) => {
-        if(ele?.ID?.consultancy){
-            ele.ID.consultancy?.map((consultancy) => {
-                consultancies.push(consultancy);
-            })
-        }
-    })
+    // Directly use data since it should be an array of consultancy objects
+    let consultancies = [];
+    
+    // Check if data exists and is an array
+    if (data && Array.isArray(data)) {
+        consultancies = data;
+    }
         
     return (
         <div className='w-[96%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-2 mx-auto my-[60px] pt-[54px] place-items-center'>
             <Heading name="Consultancy" />
-            {!loading?<Table tablehead={tablehead} data={consultancies}/>:<Loading/>}
+            {!loading ? <Table tablehead={tablehead} data={consultancies}/> : <Loading/>}
         </div>
     )
 }
