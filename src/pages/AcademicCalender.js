4076c550919a72f2e3aba7559bd3e6b6907f6a1e
nitjofficial <78634968+nitjofficial@users.noreply.github.com>
2026-06-03 10:29:51 +0530

    Update AcademicCalender.js

--- src/pages/AcademicCalender.js
+++ src/pages/AcademicCalender.js
@@ -11,7 +11,9 @@ function AcademicCalender() {
     return (
         <div className='w-[98%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-2 mx-1 xl:mx-3 my-[60px] pt-[54px] place-items-center'>
             <Heading name="Department Activities Calendar" />
-            <div className='shadow shadow-blue-400 md:m-4 pb-2'>
+            <div className="shadow shadow-blue-400 md:m-4 pb-2 w-full">
+
+
                 <div className='flex items-center w-full py-3 font-medium text-lg px-4 shadow-md shadow-blue-200'>
                 </div>
                 {data.length>0?<OpenPdf link={data[0]?.link} />:<Loading/>}
